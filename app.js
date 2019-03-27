43ff5481a6abfdba651970358963b9ce7ca51679
Kenn <kenn+git@code-crew.org>
2019-03-27 13:43:10 -0500

    Graded 03-27-2019 13:42:42

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ var app = express();
 
 
 //Set up mongoose connection
-var mongoose = require('mongoose');
-var mongoDB = '';
-mongoose.connect(mongoDB, { useNewUrlParser: true });
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// var mongoose = require('mongoose');
+// var mongoDB = '';
+// mongoose.connect(mongoDB, { useNewUrlParser: true });
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
 // view engine setup
